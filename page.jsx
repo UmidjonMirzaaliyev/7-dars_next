c0893af475b197f82a480dba624c2c742929d796
Umidjon Mirzaaliyev Web <115705707+UmidjonMirzaaliyev@users.noreply.github.com>
2023-11-10 07:39:48 +0500

    Add files via upload

---
+++ page.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      {/* <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="./About">About</Link>
+        </li>
+        <li>
+          <Link href="./About/Team">Our Team</Link>
+        </li>
+      </ul> */}
+    </div>
+  );
+}
+
+export default HomePage;
